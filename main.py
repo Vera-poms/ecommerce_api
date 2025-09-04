f9e0f0b82dc54822ad46828790839d864a1c1203
Vera-poms <verapomaa99@gmail.com>
2025-09-04 15:49:34 +0000

    Updated the checkout endpoint

--- main.py
+++ main.py
@@ -42,8 +42,12 @@ def post_products(product: ProductModel):
 
 @app.get("/products/{product_id}")
 def get_product_by_id(product_id):
-    product = products_collection.find_one({"_id": ObjectId(product_id)})
-    return {"product": replace_product_id(product)}
+    for product in products_collection:
+        if product["_id"] == product_id:
+            product = products_collection.find_one({"_id": ObjectId(product_id)})
+            return {"product": replace_product_id(product)}
+        else:
+            raise HTTPException(status_code=404, detail="Product not found")
 
 @app.get("/users")
 def get_users():
@@ -52,14 +56,22 @@ def get_users():
 
 @app.post("/register")
 def register_user(user: UserModel):
-    users_collection.insert_one(user.model_dump())
-    return {"message": "Registered successfully"}
+    ecommerce_user_email = users_collection.find_one({"email": user.email})
+    ecommerce_user_password = users_collection.find_one({"password": user.password})
+
+    if ecommerce_user_email or ecommerce_user_password:
+        raise HTTPException(status_code=409, detail="Email or password already in use")
+    else:
+        users_collection.insert_one(user.model_dump())
+        return {"message": "Registered successfully"}
 
 @app.post("/login")
 def login_user(user: UserModel):
     ecommerce_user_email = users_collection.find_one({"email": user.email})
 
     if ecommerce_user_email and (ecommerce_user_email["password"] != user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    elif not ecommerce_user_email:
         raise HTTPException(status_code=401, detail="Invalid credentials")
     else:
         return {"message": "Login successful"}
@@ -68,25 +80,52 @@ def login_user(user: UserModel):
 @app.post("/cart")
 def cart(item: CartModel):
     users_cart.insert_one(item.model_dump())
-    # cart_item = users_cart.find_one({"product_id": item.product_id})
 
-    # if item.quantity <= 0:
-    #     raise HTTPException(status_code=400, detail="Quantity must be at least 1")
-    # else:
+    if item.quantity <= 0:
+        raise HTTPException(status_code=400, detail="Quantity must be at least 1")
+    
     return {"message": f"{item.quantity} item(s) added to cart"}
 
-    # return {}
 
 @app.get("/cart/{user_id}")
 def get_cart(user_id: str):
-    users_cart_items = list(users_cart.find_one({"user_id": ObjectId(user_id)}))
+    users_cart_items = list(users_cart.find({"user_id": user_id}))
+    items = [replace_cart_id(item) for item in users_cart_items]
+        
+    return {"cart_items": items}
 
-    for item in users_cart_items:
-        item["_id"]
-    return {"item": list(map(replace_cart_id, users_cart_items))}
+@app.post("/checkout/{user_id}")
+def checkout(user_id: str, product: ProductModel):
+    cart_items = users_cart.find({"user_id": str(ObjectId(user_id))}).to_list()
 
-# @app.post("checkout/{user_id}")
-# def checkout(user_id: UserModel, cart: CartModel):
-#     subtotal = cart.price * cart.quantity
-#     total = subtotal * 1
-#     return {"cart_items": cart, "total": total}
+    if not cart_items:
+        raise HTTPException(status_code=404, detail="No items in cart")
+    
+    detailed_cart = []
+    subtotal = 0
+    total = 0
+    
+    for item in cart_items:
+
+        product = products_collection.find_one({"_id": ObjectId(item["product_id"])})
+        if not product: 
+            continue
+
+        price = float(item["price"])
+        quantity = int(item["quantity"])
+        subtotal += price * quantity
+        total += subtotal 
+
+        detailed_cart.append({
+            "product_id": str(product["_id"]),
+            "name": product["name"],
+            "price": price,
+            "quantity": quantity
+        })
+
+
+    return {
+            "cart_items": detailed_cart, 
+            "subtotal": subtotal,
+            "total": total
+            }
